083d151535944833db24953fc5cbbabc290e9663
Leo <l.oberndorfer@students.htl-leonding.ac.at>
2024-03-21 22:28:01 +0100

    Added satellite view

--- backend/HotviteServer/server.ts
+++ backend/HotviteServer/server.ts
@@ -1,10 +1,10 @@
-import express, {Request, Response, NextFunction} from "express"
+import express, {Request, Response, NextFunction} from "express";
 const server = express();
 
 server.use(express.json());
 
 server.listen(3001);
 
-server.get("/", (req, res) => {
+server.get("/", (req: any, res: any) => {
     res.send(200).send("Server online");
 });
