ecc978c366d8d8d9e7f3eccd564ef6a3acc9e22b
Leo-Oberndorfer <l.oberndorfer@students.htl-leonding.ac.at>
2024-04-19 12:54:02 +0200

    Added click listener

--- frontend/Hotvite/src/load-map.js
+++ frontend/Hotvite/src/load-map.js
@@ -1,6 +1,9 @@
 let map;
+let setMarker;
+// const { AdvancedMarkerElement } = async () =>  await google.maps.importLibrary("marker");
 
 async function initMap() {
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
   map = new google.maps.Map(document.getElementById("map"), {
     // 48.143168, 13.991348
     center: { lat: 48.143168, lng: 13.991348 },
@@ -21,42 +24,61 @@ async function initMap() {
   });
 
   console.log("Map loaded");
-  //initMapClickEvent(map);
-  loadEvents();
-  console.log(document.querySelector(".gm-style"));
+  loadEvents(AdvancedMarkerElement);
+  initMapClickEvent(AdvancedMarkerElement);
 }
 
 
-async function loadEvents(){
+async function loadEvents(AdvancedMarkerElement){
   requestJSON("/api/event/getAll").then((data) => {
     data.forEach((event) => {
-      createMarker(event);
+      createMarker(event, AdvancedMarkerElement);
     });
   });
 }
 
-async function createMarker(event){
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-  const img = createImgElement();
+async function createMarker(event, AdvancedMarkerElement){
+  const img = createImgElement("../assets/event/ev_icon.png");
   requestJSON(`/api/event/getLocationById/${event.location_id}`).then((location) => {
     const { latitude, longitude } = location;
     const marker = new AdvancedMarkerElement({
       position: { lat: +latitude, lng: +longitude },
-      title: event.title,
       map,
       content: img,
     });
   });
 }
 
-function createImgElement(){
+function createImgElement(imgSrc){
   const img = document.createElement("img");
-  img.src = "../assets/ev_icon.png";
+  img.src = imgSrc;
   // img.src = "https://cdn.discordapp.com/attachments/907636016674902037/1226616985413226536/2c90950f81e0533916080d2f4c8f71ad.webp?ex=66256b16&is=6612f616&hm=4560c272309319a581a433b1acac3e8f0bee79a9a558594a5fbbf0db23ab002a&;"
   img.style.width = "30px";
   img.style.height = "39px";
-  img.style.filter = "drop-shadow(0 0 5px white)"
+  img.style.filter = "drop-shadow(0 0 2px white)"
   return img;
+}
+
+async function initMapClickEvent(AdvancedMarkerElement) {
+  //const {AdvancedMarkerElement} = await google.maps.importLibrary("marker");
+
+  // Right click event
+  map.addListener("rightclick", async (event) => {
+    const lat = event.latLng.lat();
+    const lng = event.latLng.lng();
+
+    // Remove old marker from map
+    if (setMarker) {
+      setMarker.setMap(null);
+    }
+    setMarker = new AdvancedMarkerElement({
+      position: { lat, lng },
+      map,
+      content: createImgElement("../assets/event/create_event_icon.png"),
+    });
+
+    map.panTo({ lat, lng });
+  });
 }
 
 window.initMap = initMap;
