c440d175a7550e795b0e31fb72db6d38030a3fcd
Leo-Oberndorfer <l.oberndorfer@students.htl-leonding.ac.at>
2024-05-25 13:15:38 +0200

    Added basic functionality to requirements and the chat button

--- frontend/Hotvite/src/create-event-src.js
+++ frontend/Hotvite/src/create-event-src.js
@@ -1,26 +1,84 @@
-const input = document.querySelector('.event-req-input');
 const priceInput = document.getElementById('event-price');
+const requirementsContainer = document.getElementById('event-requirements-container');
+const addRequirementButton = document.getElementById('event-requirements-add-button');
+const toggleChatButton = document.getElementById('event-chat-button');
 
-input.addEventListener('input', function() {
-  this.style.width = '50px';
-  if(this.value.length > 0) {
-    this.style.width = (this.scrollWidth + 1) + 'px';
-  } else {
-    this.style.width = 'auto';
-  }
-});
 
+
+// Listeners
 priceInput.addEventListener("focus", function() {
   this.value = "";
 });
-
-function formatPrice() {
-  const input = document.getElementById('event-price');
-  const value = parseFloat(input.value);
+priceInput.addEventListener("blur", function() {
+  const value = parseFloat(priceInput.value);
 
   if (!isNaN(value) && value >= 0) {
-    input.value = value.toFixed(2) + '€';
+    priceInput.value = value.toFixed(2) + '€';
   } else {
-    input.value = '0.00€';
+    priceInput.value = '0.00€';
   }
+});
+addRequirementButton.addEventListener('click', createNewRequirement);
+toggleChatButton.addEventListener('click', function() {
+  const val = toggleChatButton.value;
+  if (val === "Enabled") {
+    toggleChatButton.style.backgroundColor = "var(--reject-bg-color)";
+    toggleChatButton.value = "Disabled";
+  } else {
+    toggleChatButton.style.backgroundColor = "var(--accept-bg-color)";
+    toggleChatButton.value = "Enabled";
+  }
+});
+
+
+
+// Add listeners
+function addInputChangeListener(input) {
+  input.addEventListener('input', function() {
+    this.style.width = '50px';
+    if(this.value.length > 0) {
+      this.style.width = (this.scrollWidth + 1) + 'px';
+    } else {
+      this.style.width = 'auto';
+    }
+  });
+}
+
+function addRemoveButtonListener(removeButton) {
+  removeButton.addEventListener('click', function() {
+    this.parentElement.remove();
+  });
+}
+
+
+
+// Create new requirement
+function createNewRequirement() {
+  createRequirementElement({ text: 'New Requirement' });
+}
+
+function createRequirementElement(requirement) {
+  const requirementElement = document.createElement('div');
+  const input = document.createElement('input');
+  const removeButton = document.createElement('div');
+  const img = document.createElement('img');
+
+  requirementElement.classList.add('event-requirement');
+  input.classList.add('event-req-input');
+  input.setAttribute('maxlength', '30');
+  input.setAttribute('type', 'text');
+  input.setAttribute('placeholder', requirement.text);
+  addInputChangeListener(input);
+
+  removeButton.classList.add('event-requirement-remove-button');
+  img.setAttribute('src', '../assets/event/close.png');
+  img.setAttribute('alt', 'Remove');
+  img.setAttribute('width', '13');
+  img.setAttribute('height', '13');
+  removeButton.appendChild(img);
+  addRemoveButtonListener(removeButton);
+
+  requirementElement.appendChild(input);
+  requirementElement.appendChild(removeButton);
+  requirementsContainer.insertBefore(requirementElement, addRequirementButton);
 }
