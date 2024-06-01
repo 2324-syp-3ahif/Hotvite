8587e45a678577cec81bcd0ad59ceb75132e7e7b
Leo-Oberndorfer <l.oberndorfer@students.htl-leonding.ac.at>
2024-06-02 01:21:22 +0200

    Login finally works

---
+++ frontend/Hotvite/src/portal.js
@@ -0,0 +1,45 @@
+function openPortal() {
+  const popup = document.getElementById('portal-popup');
+  popup.style.display = "block";
+  popup.contentWindow.document.addEventListener('click', function(event) {
+    if (event.target === popup.contentWindow.document.body) {
+      closePortal();
+    }
+  });
+}
+
+function closePortal() {
+  const popup = document.getElementById('portal-popup');
+  popup.style.display = "none";
+}
+
+function closeMe() {
+  window.parent.closePortal();
+}
+
+function sendLoginRequest(formElement) {
+  const email = formElement.querySelector('#email-input').value;
+  const password = formElement.querySelector('#password-input').value;
+
+  const user = {
+    email: email,
+    password: password
+  };
+
+  fetch('http://localhost:3000/api/user/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  })
+    .then(response => response.json())
+    .then(data => {
+      localStorage.setItem("token", data.token);
+      closeMe();
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      // Now do something with the error
+    });
+}
