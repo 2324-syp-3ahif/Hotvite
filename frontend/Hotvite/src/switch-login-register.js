7e29197abba5e5f7e90ef0320ab5dc126d9f0031
Lisa Wieshofer <114491336+LisaWieshofer@users.noreply.github.com>
2024-06-01 15:33:13 +0200

    switch-login-register

---
+++ frontend/Hotvite/src/switch-login-register.js
@@ -0,0 +1,9 @@
+function switchToRegister() {
+  console.log("change_page");
+  window.location.href = "register.html";
+}
+
+function switchToLogin() {
+  console.log("change_page");
+  window.location.href = "login.html";
+}
