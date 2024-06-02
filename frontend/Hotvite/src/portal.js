9c100f7e129171c9c4a091da069fdae4cb677064
Lisa Wieshofer <114491336+LisaWieshofer@users.noreply.github.com>
2024-06-02 20:04:37 +0200

    register

--- frontend/Hotvite/src/portal.js
+++ frontend/Hotvite/src/portal.js
@@ -1,7 +1,7 @@
 function openPortal() {
   const popup = document.getElementById('portal-popup');
   popup.style.display = "block";
-  popup.contentWindow.document.addEventListener('click', function(event) {
+  popup.contentWindow.document.addEventListener('click', function (event) {
     if (event.target === popup.contentWindow.document.body) {
       closePortal();
     }
@@ -41,5 +41,32 @@ function sendLoginRequest(formElement) {
     .catch(error => {
       console.error('Error:', error);
       // Now do something with the error
+    });
+}
+
+function sendRegisterRequest(formElement) {
+  const username = formElement.querySelector('#username-input').value;
+  const email = formElement.querySelector('#email-input').value;
+  const password = formElement.querySelector('#password-input').value;
+
+  const user = {
+    username: username,
+    email: email,
+    password: password
+  };
+
+  fetch('http://localhost:3000/api/user/register', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Registration successful:', data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
     });
 }
